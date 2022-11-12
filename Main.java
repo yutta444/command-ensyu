fa70547cb9f6b414f3890637d6188e99b33a8698
sekimukai <y.sekimukai.sys22@morijyobi.ac.jp>
2022-11-12 20:56:35 +0900

    間違ったコミットメッセージ

--- Main.java
+++ Main.java
@@ -2,14 +2,22 @@ import java.util.Scanner;
 public class Main{
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		double height,weight;
-		System.out.print("身長:");
-		height = s.nextDouble();
-		System.out.print("体重:");
+		double height,height2,weight,weight2;
+                System.out.print("１人目の身長:");
+                height = s.nextDouble();
+                System.out.print("１人目の体重:");
                 weight = s.nextDouble();
-		double bmi = weight / (height * height);
+		iSystem.out.print("２人目の身長:");
+                height2 = s.nextDouble();
+                System.out.print("２人目の体重:");
+                weight2 = s.nextDouble();
+                double bmi = weight / (height * height);
                 String bmi1 = String.format("%.2f",bmi);
                 double BMI = Double.parseDouble(bmi1);
-		System.out.println(BMI);
-	}
+		double bmi2 = weight2 / (height2 * height2);
+		String bmi3 = String.format("%.2f",bmi2);
+		double BMI1 = Double.parseDouble(bmi3);
+                System.out.println(BMI);
+	        System.out.println(BMI1);
+    }
 }
